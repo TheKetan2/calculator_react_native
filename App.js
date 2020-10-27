997fcae83c5ef86647a750cfea440d63b22fe4db
Ketan <ketantoo@gmail.com>
2020-10-27 12:50:33 +0530

    number input working

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, StatusBar, SafeAreaView } from "react-native";
 
 import Row from "./components/Row";
 import Button from "./components/Button";
 
 export default function App() {
+  const [currentValue, setCurrentValue] = useState("0");
+  const handleClick = (type, value) => {
+    if (currentValue.includes(".") && value === ".") return;
+    if (type === "num") {
+      let temp = "";
+      if (currentValue === "0") {
+        temp = value;
+        setCurrentValue(temp);
+      } else {
+        temp = currentValue + value;
+        setCurrentValue(temp);
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <SafeAreaView>
         <StatusBar barStyle={"light-content"} />
-        <Text style={styles.value}>100</Text>
+        <Text style={styles.value}>
+          {parseFloat(currentValue).toLocaleString()}
+        </Text>
         <Row>
           <Button theme={"secondary"} onPress={() => {}} text={"C"} />
           <Button theme={"secondary"} onPress={() => {}} text={"+/-"} />
@@ -17,26 +34,30 @@ export default function App() {
           <Button theme={"accent"} onPress={() => {}} text={"/"} />
         </Row>
         <Row>
-          <Button onPress={() => {}} text={"7"} />
-          <Button onPress={() => {}} text={"8"} />
-          <Button onPress={() => {}} text={"9"} />
+          <Button onPress={() => handleClick("num", "7")} text={"7"} />
+          <Button onPress={() => handleClick("num", "8")} text={"8"} />
+          <Button onPress={() => handleClick("num", "9")} text={"9"} />
           <Button theme={"accent"} onPress={() => {}} text={"x"} />
         </Row>
         <Row>
-          <Button onPress={() => {}} text={"4"} />
-          <Button onPress={() => {}} text={"5"} />
-          <Button onPress={() => {}} text={"6"} />
+          <Button onPress={() => handleClick("num", "4")} text={"4"} />
+          <Button onPress={() => handleClick("num", "5")} text={"5"} />
+          <Button onPress={() => handleClick("num", "6")} text={"6"} />
           <Button theme={"accent"} onPress={() => {}} text={"-"} />
         </Row>
         <Row>
-          <Button onPress={() => {}} text={"1"} />
-          <Button onPress={() => {}} text={"2"} />
-          <Button onPress={() => {}} text={"3"} />
+          <Button onPress={() => handleClick("num", "1")} text={"1"} />
+          <Button onPress={() => handleClick("num", "2")} text={"2"} />
+          <Button onPress={() => handleClick("num", "3")} text={"3"} />
           <Button theme={"accent"} onPress={() => {}} text={"+"} />
         </Row>
         <Row>
-          <Button size={"double"} onPress={() => {}} text={"0"} />
-          <Button onPress={() => {}} text={"."} />
+          <Button
+            size={"double"}
+            onPress={() => handleClick("num", "0")}
+            text={"0"}
+          />
+          <Button onPress={() => handleClick("num", ".")} text={"."} />
           <Button theme={"accent"} onPress={() => {}} text={"="} />
         </Row>
       </SafeAreaView>
