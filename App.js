5a34cdecd9ab90f0dda5c3b992dd25dfb5359849
Ketan <ketantoo@gmail.com>
2020-10-27 13:34:10 +0530

    calculaor finished

--- App.js
+++ App.js
@@ -6,17 +6,60 @@ import Button from "./components/Button";
 
 export default function App() {
   const [currentValue, setCurrentValue] = useState("0");
+  const [prev, setPrev] = useState(null);
+  const [operator, setOperator] = useState(null);
   const handleClick = (type, value) => {
     if (currentValue.includes(".") && value === ".") return;
     if (type === "num") {
       let temp = "";
       if (currentValue === "0") {
-        temp = value;
-        setCurrentValue(temp);
+        if (value === ".") {
+          setCurrentValue("0" + value);
+        } else {
+          temp = value;
+          setCurrentValue(temp);
+        }
       } else {
         temp = currentValue + value;
         setCurrentValue(temp);
       }
+    }
+
+    if (type === "operator") {
+      setPrev(currentValue);
+      setOperator(value);
+      setCurrentValue("0");
+    }
+
+    if (type === "equal") {
+      const current = parseFloat(currentValue);
+      const previous = parseFloat(prev);
+      if (operator === "+") {
+        setCurrentValue((current + previous).toString());
+        setOperator(null);
+        setPrev(null);
+      }
+      if (operator === "-") {
+        setCurrentValue((current - previous).toString());
+        setOperator(null);
+        setPrev(null);
+      }
+      if (operator === "*") {
+        setCurrentValue((current * previous).toString());
+        setOperator(null);
+        setPrev(null);
+      }
+      if (operator === "/") {
+        setCurrentValue((current / previous).toString());
+        setOperator(null);
+        setPrev(null);
+      }
+    }
+
+    if (type === "clear") {
+      setCurrentValue("0");
+      setOperator(null);
+      setPrev(0);
     }
   };
 
@@ -24,32 +67,53 @@ export default function App() {
     <View style={styles.container}>
       <SafeAreaView>
         <StatusBar barStyle={"light-content"} />
+        {/* <Text style={styles.value}>{prev + operator}</Text> */}
         <Text style={styles.value}>
           {parseFloat(currentValue).toLocaleString()}
         </Text>
         <Row>
-          <Button theme={"secondary"} onPress={() => {}} text={"C"} />
+          <Button
+            theme={"secondary"}
+            onPress={() => handleClick("clear", "")}
+            text={"C"}
+          />
           <Button theme={"secondary"} onPress={() => {}} text={"+/-"} />
           <Button theme={"secondary"} onPress={() => {}} text={"%"} />
-          <Button theme={"accent"} onPress={() => {}} text={"/"} />
+          <Button
+            theme={"accent"}
+            onPress={() => handleClick("operator", "/")}
+            text={"/"}
+          />
         </Row>
         <Row>
           <Button onPress={() => handleClick("num", "7")} text={"7"} />
           <Button onPress={() => handleClick("num", "8")} text={"8"} />
           <Button onPress={() => handleClick("num", "9")} text={"9"} />
-          <Button theme={"accent"} onPress={() => {}} text={"x"} />
+          <Button
+            theme={"accent"}
+            onPress={() => handleClick("operator", "*")}
+            text={"x"}
+          />
         </Row>
         <Row>
           <Button onPress={() => handleClick("num", "4")} text={"4"} />
           <Button onPress={() => handleClick("num", "5")} text={"5"} />
           <Button onPress={() => handleClick("num", "6")} text={"6"} />
-          <Button theme={"accent"} onPress={() => {}} text={"-"} />
+          <Button
+            theme={"accent"}
+            onPress={() => handleClick("operator", "-")}
+            text={"-"}
+          />
         </Row>
         <Row>
           <Button onPress={() => handleClick("num", "1")} text={"1"} />
           <Button onPress={() => handleClick("num", "2")} text={"2"} />
           <Button onPress={() => handleClick("num", "3")} text={"3"} />
-          <Button theme={"accent"} onPress={() => {}} text={"+"} />
+          <Button
+            theme={"accent"}
+            onPress={() => handleClick("operator", "+")}
+            text={"+"}
+          />
         </Row>
         <Row>
           <Button
@@ -58,7 +122,11 @@ export default function App() {
             text={"0"}
           />
           <Button onPress={() => handleClick("num", ".")} text={"."} />
-          <Button theme={"accent"} onPress={() => {}} text={"="} />
+          <Button
+            theme={"accent"}
+            onPress={() => handleClick("equal", "=")}
+            text={"="}
+          />
         </Row>
       </SafeAreaView>
       <StatusBar style="auto" />
